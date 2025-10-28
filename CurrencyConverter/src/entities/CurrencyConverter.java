58c74ed280d3085ed46f6ba2b717fff0b1c91837
dev-davimarques <davimarques10.dm@icloud.com>
2025-10-27 23:33:40 -0300

    fixing

--- CurrencyConverter/src/entities/CurrencyConverter.java
+++ CurrencyConverter/src/entities/CurrencyConverter.java
@@ -2,14 +2,14 @@ package entities;
 
 public class CurrencyConverter {
     private static double DOLAR = 3.10;
-    private double iof = 0.06;
+    private static double iof = 0.06;
 
     public static double getDOLAR() {
         return DOLAR;
     }
 
     public static double dolarEmReal(double valor){
-        return getDOLAR() * (valor * (1 + 0.06));
+        return getDOLAR() * (valor * (1 + iof));
     }
 
 }
